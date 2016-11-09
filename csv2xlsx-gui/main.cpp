883be42414d8e753b8ac87a74c2dce164cd802ad
OKAMURA, Yasunobu <okamura@informationsea.info>
2016-11-09 23:45:40 +0900

    show error when failed

--- csv2xlsx-gui/main.cpp
+++ csv2xlsx-gui/main.cpp
@@ -11,10 +11,16 @@
 #include <wx/filedlg.h>
 #include <wx/file.h>
 #include <wx/filename.h>
+#include <wx/msgdlg.h>
 
 #include <stdio.h>
 
 #include "libcsv2xlsx.h"
+
+#define FILE_CREATE_TRY_COUNT 100
+
+wxDECLARE_EVENT(wxEVT_COMMAND_CONVERT_COMPLETE, wxThreadEvent);
+wxDECLARE_EVENT(wxEVT_COMMAND_CONVERT_ERROR, wxThreadEvent);
 
 class ProgressFrame;
 
@@ -35,22 +41,29 @@ public:
 	ProgressFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
 
 	void StartConvert(const wxString &path);
+	void OnThreadUpdate(wxCommandEvent &event);
+	void OnThreadError(wxCommandEvent &event);
 
 private:
 	void OnExit(wxCommandEvent& event);
 	virtual wxThread::ExitCode Entry();
-	void OnThreadUpdate(wxThreadEvent& evt);
 
 	wxGauge *progress;
 	wxString path;
 	wxString newFileName;
+	wxString errorReason;
 	wxCriticalSection critical;
 	wxDECLARE_EVENT_TABLE();
 };
 
 wxBEGIN_EVENT_TABLE(ProgressFrame, wxFrame)
 EVT_MENU(wxID_EXIT, ProgressFrame::OnExit)
+EVT_COMMAND(wxID_ANY, wxEVT_COMMAND_CONVERT_COMPLETE, ProgressFrame::OnThreadUpdate)
+EVT_COMMAND(wxID_ANY, wxEVT_COMMAND_CONVERT_ERROR, ProgressFrame::OnThreadError)
 wxEND_EVENT_TABLE()
+
+wxDEFINE_EVENT(wxEVT_COMMAND_CONVERT_COMPLETE, wxThreadEvent);
+wxDEFINE_EVENT(wxEVT_COMMAND_CONVERT_ERROR, wxThreadEvent);
 
 wxIMPLEMENT_APP(MyApp);
 
@@ -77,7 +90,7 @@ bool MyApp::OnCmdLineParsed(wxCmdLineParser & parser)
 
 bool MyApp::OnCmdLineError(wxCmdLineParser & parser)
 {
-	wxFileDialog openFileDialog((wxWindow*)0, _("Open CSV/TSV file"), "", "", "CSV/TSV files (*.tsv,*.txt,*.csv)|"
+	wxFileDialog openFileDialog(frame, _("Open CSV/TSV file"), "", "", "CSV/TSV files (*.tsv,*.txt,*.csv)|"
 		"*.tsv;*.txt;*.csv", wxFD_OPEN | wxFD_FILE_MUST_EXIST);
 	if (openFileDialog.ShowModal() == wxID_CANCEL)
 		return false;
@@ -96,7 +109,7 @@ ProgressFrame::ProgressFrame(const wxString& title, const wxPoint& pos, const wx
 
 	SetMenuBar(menuBar);
 
-	Bind(wxEVT_THREAD, &ProgressFrame::OnThreadUpdate, this);
+//	Bind(wxEVT_THREAD, &ProgressFrame::OnThreadUpdate, this);
 
 	progress = new wxGauge(this, (int)PROGRESS_WINDOW, 100);
 	progress->Pulse();
@@ -135,40 +148,61 @@ wxThread::ExitCode ProgressFrame::Entry()
 	newFileName = wxString::Format("%s.xlsx", basename);
 	
 	if (!writeFile.Create(newFileName)) {
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < FILE_CREATE_TRY_COUNT; i++) {
 			newFileName = wxString::Format("%s-%d.xlsx", basename, i+1);
 			if (writeFile.Create(newFileName)) break;
 		}
+	}
+
+	if (!writeFile.IsOpened()) {
+		wxQueueEvent(GetEventHandler(), new wxThreadEvent(wxEVT_COMMAND_CONVERT_ERROR));
+		errorReason = "Cannot create new xlsx file";
+		return wxThread::ExitCode(1);
 	}
 
 	writeFile.Close();
 
 	FILE *input = fopen(path.mb_str(), "r");
 	if (input == NULL) {
+		wxQueueEvent(GetEventHandler(), new wxThreadEvent(wxEVT_COMMAND_CONVERT_ERROR));
 		wxLogError("Cannot open input file");
+		errorReason = "Cannot open input file";
 		// raise error
 		return wxThread::ExitCode(1);
 	}
 
+	bool success;
 	if (path.EndsWith(".csv"))
-		csv2xlsx(input, newFileName.mb_str());
+		success = csv2xlsx(input, newFileName.mb_str());
 	else
-		csv2xlsx_with_delimiter_and_quote(input, newFileName.mb_str(), '\t', 0);
-
+		success = csv2xlsx_with_delimiter_and_quote(input, newFileName.mb_str(), '\t', 0);
 	fclose(input);
 
+	if (!success) {
+		wxQueueEvent(GetEventHandler(), new wxThreadEvent(wxEVT_COMMAND_CONVERT_ERROR));
+		errorReason = "Cannot convert file";
+		return wxThread::ExitCode(1);
+	}
+
 	wxLogInfo("done");
-	wxQueueEvent(GetEventHandler(), new wxThreadEvent());
+	wxQueueEvent(GetEventHandler(), new wxThreadEvent(wxEVT_COMMAND_CONVERT_COMPLETE));
 	return wxThread::ExitCode(0);
 }
 
 #include <Windows.h>
 
-void ProgressFrame::OnThreadUpdate(wxThreadEvent & evt)
+void ProgressFrame::OnThreadUpdate(wxCommandEvent &event)
 {
 	wxCriticalSectionLocker locker(critical);
 	wxLogInfo("thread ok!");
 	ShellExecute(0, _("open"), newFileName.fn_str(), NULL, NULL, SW_SHOW);
+	Close();
+}
+
+void ProgressFrame::OnThreadError(wxCommandEvent &event)
+{
+	wxMessageDialog messageBox(this, errorReason, _("Error"), wxOK);
+	messageBox.ShowWindowModal();
 	Close();
 }
 
