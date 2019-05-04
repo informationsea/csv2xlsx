36dc4f9f845c64492e76ec7aa75bc94de54eacc0
OKAMURA, Yasunobu <okamura@informationsea.info>
2019-05-04 12:18:31 +0900

    fix libcsv bug

--- libcsv/libcsv.c
+++ libcsv/libcsv.c
@@ -22,14 +22,17 @@
 
 #define DEFAULT_BUFFER_SIZE 1024
 
-enum CSV_READER_MODE {
-	NORMAL, IN_QUOTE, AFTER_QUOTE
+enum CSV_READER_MODE
+{
+	NORMAL,
+	IN_QUOTE,
+	AFTER_QUOTE
 };
 
-struct csv_reader* csv_reader_initialize(FILE *file, char delimiter, char quote_char)
+struct csv_reader *csv_reader_initialize(FILE *file, char delimiter, char quote_char)
 {
-	struct csv_reader* reader = malloc(sizeof(struct csv_reader));
-	memset(reader, 0, sizeof (struct csv_reader));
+	struct csv_reader *reader = malloc(sizeof(struct csv_reader));
+	memset(reader, 0, sizeof(struct csv_reader));
 	reader->file = file;
 	reader->delimiter = delimiter;
 	reader->quote_char = quote_char;
@@ -39,8 +42,10 @@ struct csv_reader* csv_reader_initialize(FILE *file, char delimiter, char quote_
 	return reader;
 }
 
-static size_t reader_add_charactor(struct csv_reader* reader, char ch, size_t current_size) {
-	if (reader->current_buffer_size <= current_size) {
+static size_t reader_add_charactor(struct csv_reader *reader, char ch, size_t current_size)
+{
+	if (reader->current_buffer_size <= current_size)
+	{
 		char *buf = realloc(reader->buf, reader->current_buffer_size + DEFAULT_BUFFER_SIZE);
 		if (buf == NULL)
 			return 0;
@@ -51,65 +56,86 @@ static size_t reader_add_charactor(struct csv_reader* reader, char ch, size_t cu
 	return current_size;
 }
 
-const char *csv_read_next(struct csv_reader* reader, bool *lineend)
+const char *csv_read_next(struct csv_reader *reader, bool *lineend)
 {
 	enum CSV_READER_MODE mode = NORMAL;
 	size_t current_size = 0;
 	*lineend = false;
 	bool finish_flag = false;
-	do {
+	do
+	{
 		int ch = fgetc(reader->file);
-		if (ch < 0) {
+		if (ch < 0)
+		{
 			finish_flag = true;
 			break;
 		}
 
-		switch (mode) {
-		case NORMAL: {
-			if (ch == reader->delimiter) {
+		switch (mode)
+		{
+		case NORMAL:
+		{
+			if (ch == reader->delimiter)
+			{
 				reader->last_charator_is_return = false;
 				goto finish;
 			}
-			else if (ch == '\n' && current_size == 0 && reader->last_charator_is_return) {
+			else if (ch == '\n' && current_size == 0 && reader->last_charator_is_return)
+			{
 				continue; // skip
 			}
-			else if (ch == '\r' || ch == '\n') {
+			else if (ch == '\r' || ch == '\n')
+			{
 				reader->last_charator_is_return = false;
 				*lineend = true;
-				if (ch == '\r') reader->last_charator_is_return = true;
+				if (ch == '\r')
+					reader->last_charator_is_return = true;
 				goto finish;
 			}
-			else if (ch == reader->quote_char && current_size == 0) {
+			else if (ch == reader->quote_char && current_size == 0)
+			{
 				reader->last_charator_is_return = false;
 				mode = IN_QUOTE;
 			}
-			else {
+			else
+			{
 				reader->last_charator_is_return = false;
 				current_size = reader_add_charactor(reader, ch, current_size);
-				if (current_size == 0) return 0; // Error
+				if (current_size == 0)
+					return 0; // Error
 			}
 			break;
 		}
 		case IN_QUOTE:
-			if (ch == reader->quote_char) {
+			if (ch == reader->quote_char)
+			{
 				mode = AFTER_QUOTE;
 			}
-			else {
+			else
+			{
 				current_size = reader_add_charactor(reader, ch, current_size);
-				if (current_size == 0) return 0; // Error
+				if (current_size == 0)
+					return 0; // Error
 			}
 			break;
 		case AFTER_QUOTE:
-			if (ch == reader->quote_char) {
+			if (ch == reader->quote_char)
+			{
 				mode = IN_QUOTE;
 				current_size = reader_add_charactor(reader, ch, current_size);
-				if (current_size == 0) return 0; // Error
+				if (current_size == 0)
+					return 0; // Error
 			}
-			else if (ch == '\r' || ch == '\n') {
+			else if (ch == '\r' || ch == '\n')
+			{
+				reader->last_charator_is_return = false;
 				*lineend = true;
+				if (ch == '\r')
+					reader->last_charator_is_return = true;
 				goto finish;
 			}
-			else if (ch == reader->delimiter) {
+			else if (ch == reader->delimiter)
+			{
 				goto finish;
 			}
 			break;
